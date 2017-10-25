267837798134707791d6f2231d13d7754c7d620a
Daniel Opitz <git@copynpaste.de>
2017-10-26 00:19:14 +0200

    better registerScript

--- news_faviconum/appinfo/app.php
+++ news_faviconum/appinfo/app.php
@@ -1,5 +1,5 @@
 <?php
 
-if (\OCP\App::isEnabled('news') && class_exists('OCA\News\Plugin\Client\Plugin')) {
+if (\OC::$server->getAppManager()->isEnabledForUser('news') && class_exists(\OCA\News\Plugin\Client\Plugin::class)) {
     \OCA\News\Plugin\Client\Plugin::registerScript('news_faviconum', 'faviconum');
 }
